Robust pre input data normalization. More robust test data analysis / output visualization. Fix learning schedule and model weight saving

download_data.py
@@ -50,7 +50,7 @@ def pull_ticker_data(ticker):
         data = data.drop(["ticker"], axis=1)
 
         logging.info(f"Downloading: {ticker}, Price Variance: {np.std(data['close'])}")
-        if np.std(data["close"]) == 0.0:
+        if np.std(data["close"]) <= 0.5:
             return None
 
         data = data.to_dict(orient="list")
@@ -111,7 +111,7 @@ if __name__ == "__main__":
     print(one_year)
 
     tickers = yf.tickers_nasdaq()
-    inds = np.random.uniform(0.0, len(tickers), 250)
+    inds = np.random.uniform(0.0, len(tickers), 650)
     inds = [int(x) for x in inds]
     tickers = [tickers[ind] for ind in inds]  # Debug flag application
 
@@ -125,8 +125,8 @@ if __name__ == "__main__":
     list_dfs = list(filter(lambda item: item is not None, list_dfs))
 
     # Train test inf splitting
-    train = list_dfs[:200]
-    test = list_dfs[200:]
+    train = list_dfs[:450]
+    test = list_dfs[450:]
 
     download_end = timer()
     print(f"Downloading ticker data took: {round(download_end - download_start, 2)} s")

src/plotting.py
@@ -42,10 +42,18 @@ def gen_plot(
             alpha=0.3,
         )
 
+        plt.plot(
+            list(range(30, 30 + prediction_length)),
+            np.mean(outputs[idx], axis=0),
+            label="Prediction mean",
+            color="purple",
+            alpha=0.3,
+        )
+
         plt.fill_between(
             list(range(30, 30 + prediction_length)),
-            outputs[idx].mean(0) - outputs[idx].std(axis=0),
-            outputs[idx].mean(0) + outputs[idx].std(axis=0),
+            outputs[idx].mean(0) - (0.5 * outputs[idx].std(axis=0)),
+            outputs[idx].mean(0) + (0.5 * outputs[idx].std(axis=0)),
             color="orange",
             alpha=0.3,
             interpolate=True,
@@ -54,3 +62,4 @@ def gen_plot(
 
         plt.legend()
         plt.savefig(save_loc)
+        plt.close()

train/train.py
@@ -40,7 +40,7 @@ def train_model(
     use_tb: Optional[bool] = False,
     use_test: Optional[bool] = False,
     logger: Optional[logging.Logger] = None,
-    epochs: Optional[int] = 90,
+    epochs: Optional[int] = 301,
     batch_size: Optional[int] = 64,
     num_batches_per_epoch: Optional[
         int
@@ -67,7 +67,7 @@ def train_model(
     model, optim, train_dl = accelerator.prepare(model, optim, train_dl)
 
     # lr scheduling
-    sched = ExponentialLR(optim, 0.975)
+    sched = ExponentialLR(optim, 0.9725)
 
     # setup tracking / loggers for model
 
@@ -84,6 +84,14 @@ def train_model(
 
             # zero the optim gradients
             optim.zero_grad()
+
+            # normalize the input according to xi-median / IQR ()
+            q75, q25 = np.percentile(batch["past_values"].cpu().numpy(), [75, 25])
+            iqr = q75 - q25
+
+            batch["past_values"] = (
+                batch["past_values"] - T.median(batch["past_values"])
+            ) / iqr
 
             # send batch to device
             args = {
@@ -105,7 +113,7 @@ def train_model(
             losses.append(loss.item())
 
             databar.set_description(
-                f"Epoch: {epoch_num}, Iteration: {idx} / {num_batches_per_epoch} | {round(100*(idx/num_batches_per_epoch), 2)}%, Loss: {loss}"
+                f"Epoch: {epoch_num}, Iteration: {idx} / {num_batches_per_epoch} | {round(100*(idx/num_batches_per_epoch), 2)}%, Loss: {loss}, IQR: {iqr}"
             )
 
             # backprop
@@ -142,8 +150,15 @@ def train_model(
                         f"Skipping print as past_values only have variance of: {stddev_test}"
                     )
                     continue
+
+                q75, q25 = np.percentile(batch["past_values"].cpu().numpy(), [75, 25])
+                iqr = q75 - q25
+
+                past_vals = (
+                    batch["past_values"] - T.median(batch["past_values"])
+                ) / iqr
                 args = {
-                    "past_values": batch["past_values"].to(device),
+                    "past_values": past_vals.to(device),
                     "past_time_features": batch["past_time_features"].to(device),
                     "past_observed_mask": batch["past_observed_mask"].to(device),
                     "static_categorical_features": batch[